047e2624dff5338bc0efc39767a9ffc1a255f59d
Joseph <jotienoochieng@gmail.com>
2023-10-04 22:35:31 +0300

    created components, changed page layout

--- task-manager/components/Footer.tsx
+++ task-manager/components/Footer.tsx
@@ -1,7 +1,38 @@
 "use client";
 
+import { footerLinks } from "@/constants";
+import Image from "next/image";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="flex flex-col text-black-100  mt-5 border-t border-gray-100">
+      <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+        <div className=" flex flex-col j items-start gap-6">
+          <Image
+            src="./next.svg"
+            alt="footer logo"
+            height={118}
+            width={80}
+            className=" object-contain"
+          />
+          <p className=" text-black">
+            Task Manager App <br />
+            all right reserved &copy;
+          </p>
+        </div>
+        <div className=" flex flex-col gap-6 text-base min-w-[170px] ">
+          {footerLinks.map((item) => (
+            <div
+              className=" flex flex-1 w-full md:justify-end flex-wrap max-md:mt-10 gap-20
+            "
+            >
+              <h1>{item.title} </h1>
+            </div>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- task-manager/components/NavBar.tsx
+++ task-manager/components/NavBar.tsx
@@ -1,7 +1,26 @@
 "use client";
 
+import Image from "next/image";
+import Link from "next/link";
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <header className="w-full absolute z-10">
+      <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
+        <Link href="/" className="flex justify-center items-center">
+          <Image
+            src="./next.svg"
+            alt="logo"
+            height={118}
+            width={100}
+            className=" object-contain"
+          />
+        </Link>
+
+        <button>click</button>
+      </nav>
+    </header>
+  );
 };
 
 export default NavBar;
