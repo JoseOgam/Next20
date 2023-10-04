ab1d37f7da2ebaf7eccb820cadd16389492f1597
Joseph <jotienoochieng@gmail.com>
2023-10-04 07:20:47 +0300

    created components, changed page layout

--- task-manager/components/AddTask.tsx
+++ task-manager/components/AddTask.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const AddTask = () => {
-  return <div>AddTask</div>;
+  return (
+    <div className="flex-1 pt-36">
+      <form className=" flex flex-col justify-center items-center gap-3">
+        <input type="text" placeholder="task title" className=" title_input" />
+        <textarea placeholder="task desc..." className="text_area" />
+      </form>
+    </div>
+  );
 };
 
 export default AddTask;
