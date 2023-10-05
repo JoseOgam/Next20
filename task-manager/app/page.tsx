a51a068351a29303141cb7dc6448e427050a8d3d
Joseph <jotienoochieng@gmail.com>
2023-10-05 18:15:17 +0300

    connect listTask component with redux

--- task-manager/app/page.tsx
+++ task-manager/app/page.tsx
@@ -1,12 +1,17 @@
+"use client";
 import { AddTask, ListTask } from "@/components";
+import { Provider } from "react-redux";
 import Image from "next/image";
+import { store } from "@/services/store";
 
 export default function Home() {
   return (
     <main className="flex justify-center items-center p-10">
       <div>
-        <AddTask />
-        <ListTask />
+        <Provider store={store}>
+          <AddTask />
+          <ListTask />
+        </Provider>
       </div>
     </main>
   );
--- task-manager/components/ListTask.tsx
+++ task-manager/components/ListTask.tsx
@@ -1,7 +1,19 @@
+import { useAppSelector } from "@/services/hooks";
 import React from "react";
 
 const ListTask = () => {
-  return <div>ListTask</div>;
+  const tasks = useAppSelector((state: any) => state.tasks.tasks);
+  console.log(tasks);
+  return (
+    <div className=" flex-col justify-center items-center font-bold">
+      {tasks.map((item: any) => (
+        <div key={item.id}>
+          <h1>{item.title} </h1>
+          <p>{item.body} </p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ListTask;
