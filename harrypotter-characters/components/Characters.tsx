44b848cd0e447a21f4b8072ed6a90e34b2cfaf7a
Joseph <jotienoochieng@gmail.com>
2023-10-09 11:56:47 +0300

    fetched harrypotter characters from api

--- harrypotter-characters/components/Characters.tsx
+++ harrypotter-characters/components/Characters.tsx
@@ -1,6 +1,26 @@
-import React from "react";
+import { fetchChar, getCharacters } from "@/services/redux/slice";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Characters = () => {
+  const characters = useSelector((state: any) => state.characters);
+  const dispatch = useDispatch();
+  console.log(characters);
+
+  const fetchChar = async () => {
+    try {
+      const url = `https://hp-api.onrender.com/api/characters`;
+      const response = await fetch(url, {
+        method: "GET",
+      });
+      const parseRes = await response.json();
+      dispatch(getCharacters(parseRes));
+    } catch (e) {}
+  };
+
+  useEffect(() => {
+    fetchChar();
+  }, []);
   return <div>Characters</div>;
 };
 
--- harrypotter-characters/services/redux/slice.tsx
+++ harrypotter-characters/services/redux/slice.tsx
@@ -1,15 +1,50 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const charUrl = "https://hp-api.onrender.com/api/characters";
+
+export const fetchChar = createAsyncThunk("characters/fetchChar", async () => {
+  try {
+    const response = await axios.get(charUrl);
+    return response.data;
+  } catch (error) {
+    throw error; // Propagate the error
+  }
+});
+
 const initialState = {
-  characters: [{ name: "Joseph" }],
-  status: false,
+  characters: [],
+  status: "idle",
+  error: null,
 };
 
 const characterSlice = createSlice({
   name: "characters",
   initialState,
-  reducers: {},
-  extraReducers: {},
+  reducers: {
+    getCharacters: (state, { type, payload }) => {
+      return { ...state, characters: payload };
+    },
+    charDetail: (state, { type, payload }) => {
+      return { ...state, characters: payload };
+    },
+  },
+  // extraReducers: (builder) => {
+  //   builder
+  //     .addCase(fetchChar.pending, (state) => {
+  //       state.status = "loading";
+  //     })
+  //     .addCase(fetchChar.fulfilled, (state, action) => {
+  //       state.status = "succeeded";
+  //       state.characters = action.payload;
+  //     })
+  //     .addCase(fetchChar.rejected, (state: any, action) => {
+  //       state.status = "failed";
+  //       state.error = action.error.message;
+  //     });
+  // },
 });
 
+export const { getCharacters, charDetail } = characterSlice.actions;
 const charactersReducers = characterSlice.reducer;
 export default charactersReducers;
