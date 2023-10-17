8c08c8ef78be2113c69d95a0756e4b92d9955415
Joseph <jotienoochieng@gmail.com>
2023-10-17 09:01:22 +0300

    configured headless ui

---
+++ headless/components/Demo.tsx
@@ -0,0 +1,30 @@
+"use client";
+import React, { useState } from "react";
+import { people } from "@/constants";
+import { Combobox } from "@headlessui/react";
+
+const Demo = () => {
+  const [selectedPerson, setSelectedPerson] = useState(people[0]);
+  const [query, setQuery] = useState("");
+
+  const filteredPeople =
+    query === ""
+      ? people
+      : people.filter((person) => {
+          return person.toLocaleLowerCase().includes(query.toLocaleLowerCase()); // Fix the variable name here
+        });
+  return (
+    <Combobox value={selectedPerson} onChange={setSelectedPerson}>
+      <Combobox.Input onChange={(event) => setQuery(event.target.value)} />
+      <Combobox.Options>
+        {filteredPeople.map((person) => (
+          <Combobox.Option key={person} value={person}>
+            {person}
+          </Combobox.Option>
+        ))}
+      </Combobox.Options>
+    </Combobox>
+  );
+};
+
+export default Demo;
---
+++ headless/constants/index.ts
@@ -0,0 +1,8 @@
+export const people = [
+  "Wade Cooper",
+  "Durward Reynolds",
+  "Kenton Towne",
+  "Therese Wunsch",
+  "Benedict Kessler",
+  "Katelyn Rohan",
+];
