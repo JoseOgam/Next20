ba8530ace4c25142ec24870eec1e07b349fedd99
Joseph <jotienoochieng@gmail.com>
2023-10-17 10:57:39 +0300

    configured headless ui

--- headless/components/Demo.tsx
+++ headless/components/Demo.tsx
@@ -1,31 +1,31 @@
 "use client";
 import React, { Fragment, useState } from "react";
-import { people } from "@/constants";
+import { manufacturers } from "@/constants";
 import { Combobox } from "@headlessui/react";
 
 const Demo = () => {
-  const [selectedPerson, setSelectedPerson] = useState(people[0]);
+  const [selectedPerson, setSelectedPerson] = useState(manufacturers[0]);
   const [query, setQuery] = useState("");
 
   const filteredPeople =
     query === ""
-      ? people
-      : people.filter((person) => {
-          return person.toLocaleLowerCase().includes(query.toLocaleLowerCase()); // Fix the variable name here
+      ? manufacturers
+      : manufacturers.filter((car) => {
+          return car.toLocaleLowerCase().includes(query.toLocaleLowerCase()); // Fix the variable name here
         });
   return (
     <Combobox value={selectedPerson} onChange={setSelectedPerson}>
       <Combobox.Input onChange={(event) => setQuery(event.target.value)} />
       <Combobox.Options>
-        {filteredPeople.map((person) => (
-          <Combobox.Option key={person} value={person} as={Fragment}>
+        {filteredPeople.map((car) => (
+          <Combobox.Option key={car} value={car} as={Fragment}>
             {({ active }) => (
               <li
                 className={`${
                   active ? "bg-blue-500 text-white" : "bg-white text-black"
                 }`}
               >
-                {person}
+                {car}
               </li>
             )}
           </Combobox.Option>
