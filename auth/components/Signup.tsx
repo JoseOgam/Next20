c8d1fe132657a27085c8c9f37f9d76eb30b6a4e7
Joseph <jotienoochieng@gmail.com>
2023-10-25 15:49:36 +0300

    created signup component

--- auth/components/Signup.tsx
+++ auth/components/Signup.tsx
@@ -6,35 +6,39 @@ const Signup = () => {
       <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <div className="w-full md:w-full px-3 mb-6 md:mb-0">
           <input
-            placeholder="form"
+            placeholder="your name"
             className="appearance-none block w-full bg-gray-200
              text-gray-700 border border-gray-200 rounded py-3 px-4 focus:outline-none focus:bg-white focus:border-gray-500"
             type="text"
+            required
           />
         </div>
         <div className="w-full md:w-full px-3 mb-6 md:mb-0 pt-3">
           <input
-            placeholder="form"
+            placeholder="your mail"
             className="appearance-none block w-full bg-gray-200
              text-gray-700 border border-gray-200 rounded py-3 px-4 focus:outline-none focus:bg-white focus:border-gray-500"
-            type="text"
+            type="email"
+            required
           />
         </div>
 
         <div className="w-full md:w-full px-3 mb-6 md:mb-0 pt-3">
           <input
-            placeholder="form"
+            placeholder="password"
             className="appearance-none block w-full bg-gray-200
              text-gray-700 border border-gray-200 rounded py-3 px-4 focus:outline-none focus:bg-white focus:border-gray-500"
-            type="text"
+            type="password"
+            required
           />
         </div>
         <div className="w-full md:w-full px-3 mb-6 md:mb-0 pt-3">
           <input
-            placeholder="form"
+            placeholder="confirm password"
             className="appearance-none block w-full bg-gray-200
              text-gray-700 border border-gray-200 rounded py-3 px-4 focus:outline-none focus:bg-white focus:border-gray-500"
-            type="text"
+            type="password"
+            required
           />
         </div>
         <div className="flex justify-center items-center md:w-full pt-3">
