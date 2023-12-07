f9b7528f223c6ed4f8b92a6061bf60f31fcf8a11
Joseph <jotienoochieng@gmail.com>
2023-12-07 06:31:30 +0300

    created components

---
+++ portfolio/components/index.ts
@@ -0,0 +1,9 @@
+import About from "./About";
+import Contact from "./Contact";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import NavBar from "./NavBar";
+import Projects from "./Projects";
+import Skills from "./Skills";
+
+export { About, Contact, Footer, Hero, NavBar, Projects, Skills };
