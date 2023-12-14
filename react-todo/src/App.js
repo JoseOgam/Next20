631a9bc6c48124fc14bfdc1d97bcaaa6f15d9fd5
Joseph <jotienoochieng@gmail.com>
2023-12-14 09:11:55 +0300

    created reactjs project

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  return <div>App</div>;
+  const [counter, setCounter] = useState(0);
+  const add = () => {
+    setCounter(counter + 1);
+  };
+  const reset = () => {
+    setCounter(0);
+  };
+  const subtract = () => {
+    setCounter(counter - 1);
+  };
+  return (
+    <div>
+      <div>
+        <h1>{counter} </h1>
+        <button onClick={add}>add</button>
+        <button onClick={reset}>reset</button>
+        <button onClick={subtract}>subtract</button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
