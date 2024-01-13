55de1a85be4bd9de57edbf238421f5f88f79d806
Joseph <jotienoochieng@gmail.com>
2024-01-13 09:50:25 +0300

    created signup page

--- next-auth2/src/app/signup/page.tsx
+++ next-auth2/src/app/signup/page.tsx
@@ -1,9 +1,90 @@
-import React from "react";
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
 
 const Signup = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const onSignup = () => {};
   return (
     <div className=" ">
       <div className="flex justify-center items-center pt-10">Signup</div>
+      <div className=" w-full">
+        <form className="grid justify-center items-center ">
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Username
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700
+                     leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Username"
+              value={user.username}
+              onChange={(e: any) =>
+                setUser({ ...user, username: e.target.value })
+              }
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700
+                     leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="email"
+              value={user.email}
+              onChange={(e: any) => setUser({ ...user, email: e.target.value })}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700
+                     leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="password"
+              placeholder="password"
+              value={user.password}
+              onChange={(e: any) =>
+                setUser({ ...user, password: e.target.value })
+              }
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Sign Up
+            </button>
+            <Link
+              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+              href="#"
+            >
+              Forgot Password?
+            </Link>
+          </div>
+        </form>
+        <div className="mb-4">
+          <Link
+            href="/login"
+            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+          >
+            go to Login page
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
