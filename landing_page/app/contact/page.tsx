674d237ad91edb65c66900bde72cbdf3a1723b75
Joseph <jotienoochieng@gmail.com>
2024-01-29 10:25:54 +0300

    designed home page

--- landing_page/app/contact/page.tsx
+++ landing_page/app/contact/page.tsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="flex flex-col items-center justify-center pb-7">
+      <h2>Contact</h2>
+      <div className="flex items-center justify-center flex-col py-8 space-y-2">
+        <form className="flex flex-col bg-transparent space-y-5">
+          <input
+            type="text"
+            placeholder="name"
+            className=" p-3 bg-zinc-600 rounded border-black focus:border-teal-700 text-white"
+          />
+          <input
+            type="email"
+            placeholder="email"
+            className="p-3 bg-zinc-600 rounded border-black focus:border-teal-700 text-white"
+          />
+          <textarea
+            placeholder="type message here..."
+            className="p-3 bg-zinc-600 rounded border-black focus:border-teal-700 text-white resize-none"
+          />
+        </form>
+        <button className=" px-4 py-2 bg-cyan-200 rounded">Submit</button>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
