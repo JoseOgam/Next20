04e6a5bb47fb56f8e88c3a3e19538f6c3f9f0f88
Joseph <jotienoochieng@gmail.com>
2024-02-21 08:59:12 +0300

    added more search functionality

--- react-todo/src/components/ListTodo.js
+++ react-todo/src/components/ListTodo.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 
 const ListTodo = () => {
-  return <div>ListTodo</div>;
+  const [query, setQuery] = useState("");
+  const { todo, status } = useSelector((state) => state.todo);
+  console.log(todo);
+  const filteredItems = todo.filter((item) =>
+    item.task.toLowerCase().includes(query.toLowerCase())
+  );
+  return (
+    <main>
+      <div>
+        <input
+          placeholder="search task..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </div>
+      <div>
+        {filteredItems ? (
+          <div>
+            {filteredItems.map((item) => (
+              <div>
+                <h2>{item.task} </h2>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div>
+            <h2>No Task Registered</h2>
+          </div>
+        )}
+      </div>
+    </main>
+  );
 };
 
 export default ListTodo;
